f6d5f5f87fc344563ea246d329e9417a2024f177
Rudra Saraswat <rs2009@ubuntu.com>
2024-06-05 12:36:38 +0000

    feat: sleep so that message is readable

--- jade_gui/scripts/install.sh
+++ jade_gui/scripts/install.sh
@@ -2,5 +2,6 @@
 echo | tee /tmp/jade-gui-output.txt &>/dev/null
 echo "Starting installation with blend-inst..." | tee -a /tmp/jade-gui-output.txt
 echo "Errors might appear every once in a while during installation, but that's perfectly normal." | tee -a /tmp/jade-gui-output.txt
+sleep 3
 echo | tee -a /tmp/jade-gui-output.txt
 sudo blend-inst config ~/.config/jade.json | tee -a /tmp/jade-gui-output.txt
