f7e67c422fd10647973b5cbedb0a1abab51b704d
Rudra Saraswat <rs2009@ubuntu.com>
2024-06-05 07:50:07 +0000

    feat: do not hide errors

--- jade_gui/scripts/install.sh
+++ jade_gui/scripts/install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 echo | tee /tmp/jade-gui-output.txt &>/dev/null
 echo "Starting installation with blend-inst..." | tee -a /tmp/jade-gui-output.txt
-sudo blend-inst config ~/.config/jade.json 2>/dev/null | tee -a /tmp/jade-gui-output.txt
+echo "Errors might appear every once in a while during installation, but that's perfectly normal." | tee -a /tmp/jade-gui-output.txt
+echo | tee -a /tmp/jade-gui-output.txt
+sudo blend-inst config ~/.config/jade.json | tee -a /tmp/jade-gui-output.txt
